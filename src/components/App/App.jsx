3801c510bc1f1dcc122cf2ca98df1bb4e5ce3471
Adrian Fernandez <adriam.fernandez@live.com>
2024-06-08 19:56:27 -0300

    agregando componente ItemCount

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css'
 import ItemListContainer from '../ItemListConteiner/ItemListConteiner.jsx';
 import NavBar from '../NavBar/NavBar.jsx';
+import ItemCount from '../ItemCount/ItemCount.jsx'
 
 const App = () => {
   return (
     <div className="App">
-      <ItemListContainer mensajeBienvenida={"¡Bienvenidos a la pagina de fiambres mas ricos y pedidos de todo el mar del oeste!"}/>
       <NavBar/>
+      <ItemListContainer mensajeBienvenida={"¡Bienvenidos a la pagina de fiambres mas ricos y pedidos de todo el mar del oeste!"}/>
+      <ItemCount/>
     </div>
   );
 }
