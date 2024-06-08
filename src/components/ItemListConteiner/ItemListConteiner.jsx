54487ed1d34375d65141bd22c63b0536c1246ce2
Adrian Fernandez <adriam.fernandez@live.com>
2024-06-08 20:33:42 -0300

    Creando archivos de productos

--- src/components/ItemListConteiner/ItemListConteiner.jsx
+++ src/components/ItemListConteiner/ItemListConteiner.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { useState, useEffect} from 'react'
+import { getProducts } from '../../asynMock'
+
 
 const ItemListContainer = ({ mensajeBienvenida }) => {
+    const [products, setProducts] = useState ([])
+
+    useEffect(() => {
+        getProducts()
+        .then(response => {
+            setProducts(response)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }, [])
+    
+    
     return (
         <div>
             <h1>{mensajeBienvenida}</h1>
-            {/* Aquí puedes mostrar el catálogo de productos */}
+            <ItemList products={products}/>
         </div>
     );
 };
